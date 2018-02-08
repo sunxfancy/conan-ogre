26fec995c7e52493587f6f2aef95fc734b9109df
西风逍遥游 <sun_xfancy@163.com>
2018-02-07 22:12:23 -0800

    fix example

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -20,9 +20,9 @@ class TestOgreConan(ConanFile):
     requires = "OGRE/%s@%s/%s" % (version, username, channel)
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin")
